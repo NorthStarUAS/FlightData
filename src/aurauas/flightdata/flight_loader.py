874084ef4794c4739da1c89b0a188bc3a80b3595
Curtis L. Olson <curtolson@flightgear.org>
2017-09-03 15:57:54 -0500

    The loader now returns detected flight data file format.

--- src/aurauas/flightdata/flight_loader.py
+++ src/aurauas/flightdata/flight_loader.py
@@ -11,6 +11,7 @@ import umn3_mat
 
 def load(path, recal_file=None):
     flight_data = {}
+    flight_format = None
 
     (root, ext) = os.path.splitext(path)
     aura_csv_path = os.path.join(path, 'imu-0.csv')
@@ -24,31 +25,37 @@ def load(path, recal_file=None):
         # aura csv format
         print 'Detected aura csv format.'
         flight_data = aura_csv.load(path, recal_file)
+        flight_format = 'aura_csv'
     elif os.path.exists(aura_txt_path):
         # aura txt format
         print 'Detected aura text format.'
         flight_data = aura_txt.load(path, recal_file)
+        flight_format = 'aura_txt'
     elif ext == '.mat':
         # umn1, umn3
         print 'Detected umn1/umn3 format.'
         print 'Notice: assuming umn1 .mat format'
         flight_data = umn1_mat.load(path)
         #flight_data = umn3_mat.load(path)
+        flight_format = 'umn1'
     elif os.path.exists(ulog_path):
         # px4_ulog
         print 'Detected px4 ulog (csv family of files) format.'
         flight_data = px4_ulog.load(path)
+        flight_format = 'px4_ulog'
     elif ext == '.csv':
         # px4 sdlog2
         print 'Detected px4 ulog (single csv file) format.'
         flight_data = px4_sdlog2.load(path)
+        flight_format = 'px4_sdlog2'
     elif os.path.exists(sentera_path):
         # sentera1 or sentera2
         print 'Detected sentera format.'
         print 'Notice: assuming original sentera camera format'
         flight_data = sentera.load(path)
         # imu_data, gps_data, air_data, filter_data = sentera2.load(path)
+        flight_format = 'sentera'
     else:
         print "Unable to determine data log format (or path not valid):", path
 
-    return flight_data
+    return flight_data, flight_format
