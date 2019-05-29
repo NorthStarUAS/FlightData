94b176880f05dffde421c2246ce5901a98ba71d5
Curtis L. Olson <curtolson@flightgear.org>
2019-05-29 08:55:35 -0500

    Notational cleanups paired with the hdf5 generator code. Converting an hdf5 column to numpy array makes indexed access *way* faster.

--- aurauas/flightdata/aura_hdf5.py
+++ aurauas/flightdata/aura_hdf5.py
@@ -23,14 +23,14 @@ def load(h5_filename, recalibrate=None):
     # load imu/gps data files
     imucal_json = os.path.join(flight_dir, "imucal.json")
 
-    timestamp = data['/events/timestamp'][()].astype(float)
+    timestamp = data['/events/timestamp'][()]
     message = data['/events/message'][()]
     result['event'] = []
     pilot_mapping = 'Aura3'       # APM2 or Aura3
     for i in range(len(timestamp)):
         event = {
-            'time': timestamp[i][0],
-            'message': message[i][0]
+            'time': timestamp[i],
+            'message': message[i]
         }
         if 'Aura3' in event['message']:
             pilot_mapping = 'Aura3'
@@ -38,127 +38,127 @@ def load(h5_filename, recalibrate=None):
             pilot_mapping = 'APM2'
         result['event'].append(event)
         
-    timestamp = data['/sensors/imu/timestamp'][()].astype(float)
-    gx = data['/sensors/imu/p_rad_sec'][()].astype(float)
-    gy = data['/sensors/imu/q_rad_sec'][()].astype(float)
-    gz = data['/sensors/imu/r_rad_sec'][()].astype(float)
-    ax = data['/sensors/imu/ax_mps_sec'][()].astype(float)
-    ay = data['/sensors/imu/ay_mps_sec'][()].astype(float)
-    az = data['/sensors/imu/az_mps_sec'][()].astype(float)
-    hx = data['/sensors/imu/hx'][()].astype(float)
-    hy = data['/sensors/imu/hy'][()].astype(float)
-    hz = data['/sensors/imu/hz'][()].astype(float)
-    temp = data['/sensors/imu/temp_C'][()].astype(float)
+    timestamp = data['/sensors/imu/timestamp'][()]
+    gx = data['/sensors/imu/p_rad_sec'][()]
+    gy = data['/sensors/imu/q_rad_sec'][()]
+    gz = data['/sensors/imu/r_rad_sec'][()]
+    ax = data['/sensors/imu/ax_mps_sec'][()]
+    ay = data['/sensors/imu/ay_mps_sec'][()]
+    az = data['/sensors/imu/az_mps_sec'][()]
+    hx = data['/sensors/imu/hx'][()]
+    hy = data['/sensors/imu/hy'][()]
+    hz = data['/sensors/imu/hz'][()]
+    temp = data['/sensors/imu/temp_C'][()]
     result['imu'] = []
     for i in range(len(timestamp)):
         imu = {
-            'time': timestamp[i][0],
-            'p': gx[i][0],
-            'q': gy[i][0],
-            'r': gz[i][0],
-            'ax': ax[i][0],
-            'ay': ay[i][0],
-            'az': az[i][0],
-            'hx': hx[i][0],
-            'hy': hy[i][0],
-            'hz': hz[i][0],
-            'temp': temp[i][0]
+            'time': timestamp[i],
+            'p': gx[i],
+            'q': gy[i],
+            'r': gz[i],
+            'ax': ax[i],
+            'ay': ay[i],
+            'az': az[i],
+            'hx': hx[i],
+            'hy': hy[i],
+            'hz': hz[i],
+            'temp': temp[i]
         }
         result['imu'].append(imu)
 
-    timestamp = data['/sensors/gps/timestamp'][()].astype(float)
+    timestamp = data['/sensors/gps/timestamp'][()]
     unix_sec = data['/sensors/gps/unix_time_sec'][()]
     lat_deg = data['/sensors/gps/latitude_deg'][()]
     lon_deg = data['/sensors/gps/longitude_deg'][()]
-    alt = data['/sensors/gps/altitude_m'][()].astype(float)
-    vn = data['/sensors/gps/vn_ms'][()].astype(float)
-    ve = data['/sensors/gps/ve_ms'][()].astype(float)
-    vd = data['/sensors/gps/vd_ms'][()].astype(float)
+    alt = data['/sensors/gps/altitude_m'][()]
+    vn = data['/sensors/gps/vn_ms'][()]
+    ve = data['/sensors/gps/ve_ms'][()]
+    vd = data['/sensors/gps/vd_ms'][()]
     sats = data['/sensors/gps/satellites'][()]
     result['gps'] = []
     for i in range(len(timestamp)):
-        if sats[i][0] > 5:
+        if sats[i] > 5:
             gps = {
-                'time': timestamp[i][0],
-                'unix_sec': unix_sec[i][0],
-                'lat': lat_deg[i][0],
-                'lon': lon_deg[i][0],
-                'alt': alt[i][0],
-                'vn': vn[i][0],
-                've': ve[i][0],
-                'vd': vd[i][0],
-                'sats': sats[i][0]
+                'time': timestamp[i],
+                'unix_sec': unix_sec[i],
+                'lat': lat_deg[i],
+                'lon': lon_deg[i],
+                'alt': alt[i],
+                'vn': vn[i],
+                've': ve[i],
+                'vd': vd[i],
+                'sats': sats[i]
             }
             result['gps'].append(gps)
 
-    timestamp = data['/sensors/air/timestamp'][()].astype(float)
-    static_press = data['/sensors/air/pressure_mbar'][()].astype(float)
-    temp = data['/sensors/air/temp_C'][()].astype(float)
-    airspeed = data['/sensors/air/airspeed_smoothed_kt'][()].astype(float)
-    alt_press = data['/sensors/air/altitude_smoothed_m'][()].astype(float)
-    alt_true = data['/sensors/air/altitude_true_m'][()].astype(float)
-    wind_dir = data['/sensors/air/wind_dir_deg'][()].astype(float)
-    wind_speed = data['/sensors/air/wind_speed_kt'][()].astype(float)
+    timestamp = data['/sensors/air/timestamp'][()]
+    static_press = data['/sensors/air/pressure_mbar'][()]
+    temp = data['/sensors/air/temp_C'][()]
+    airspeed = data['/sensors/air/airspeed_smoothed_kt'][()]
+    alt_press = data['/sensors/air/altitude_smoothed_m'][()]
+    alt_true = data['/sensors/air/altitude_true_m'][()]
+    wind_dir = data['/sensors/air/wind_dir_deg'][()]
+    wind_speed = data['/sensors/air/wind_speed_kt'][()]
     result['air'] = []
     for i in range(len(timestamp)):
         air = {
-            'time': timestamp[i][0],
-            'static_press': static_press[i][0],
+            'time': timestamp[i],
+            'static_press': static_press[i],
             'diff_press': 0.0, # not directly available in aura flight log
-            'temp': temp[i][0],
-            'airspeed': airspeed[i][0],
-            'alt_press': alt_press[i][0],
-            'alt_true': alt_true[i][0],
-            'wind_dir': wind_dir[i][0],
-            'wind_speed': wind_speed[i][0]
+            'temp': temp[i],
+            'airspeed': airspeed[i],
+            'alt_press': alt_press[i],
+            'alt_true': alt_true[i],
+            'wind_dir': wind_dir[i],
+            'wind_speed': wind_speed[i]
         }
         result['air'].append( air )
 
-    timestamp = data['/navigation/filter/timestamp'][()].astype(float)
+    timestamp = data['/navigation/filter/timestamp'][()]
     lat = data['/navigation/filter/latitude_deg'][()]*d2r
     lon = data['/navigation/filter/longitude_deg'][()]*d2r
-    alt = data['/navigation/filter/altitude_m'][()].astype(float)
-    vn = data['/navigation/filter/vn_ms'][()].astype(float)
-    ve = data['/navigation/filter/ve_ms'][()].astype(float)
-    vd = data['/navigation/filter/vd_ms'][()].astype(float)
-    roll = data['/navigation/filter/roll_deg'][()].astype(float)
-    pitch = data['/navigation/filter/pitch_deg'][()].astype(float)
-    yaw = data['/navigation/filter/heading_deg'][()].astype(float)
-    gbx = data['/navigation/filter/p_bias'][()].astype(float)
-    gby = data['/navigation/filter/q_bias'][()].astype(float)
-    gbz = data['/navigation/filter/r_bias'][()].astype(float)
-    abx = data['/navigation/filter/ax_bias'][()].astype(float)
-    aby = data['/navigation/filter/ay_bias'][()].astype(float)
-    abz = data['/navigation/filter/az_bias'][()].astype(float)
+    alt = data['/navigation/filter/altitude_m'][()]
+    vn = data['/navigation/filter/vn_ms'][()]
+    ve = data['/navigation/filter/ve_ms'][()]
+    vd = data['/navigation/filter/vd_ms'][()]
+    roll = data['/navigation/filter/roll_deg'][()]
+    pitch = data['/navigation/filter/pitch_deg'][()]
+    yaw = data['/navigation/filter/heading_deg'][()]
+    gbx = data['/navigation/filter/p_bias'][()]
+    gby = data['/navigation/filter/q_bias'][()]
+    gbz = data['/navigation/filter/r_bias'][()]
+    abx = data['/navigation/filter/ax_bias'][()]
+    aby = data['/navigation/filter/ay_bias'][()]
+    abz = data['/navigation/filter/az_bias'][()]
     result['filter'] = []
     for i in range(len(timestamp)):
-        psi = yaw[i][0]*d2r
+        psi = yaw[i]*d2r
         if psi > math.pi:
             psi -= 2*math.pi
         if psi < -math.pi:
             psi += 2*math.pi
         psix = math.cos(psi)
         psiy = math.sin(psi)
-        if abs(lat[i][0]) > 0.0001 and abs(lon[i][0]) > 0.0001:
+        if abs(lat[i]) > 0.0001 and abs(lon[i]) > 0.0001:
             filter = {
-                'time': timestamp[i][0],
-                'lat': lat[i][0],
-                'lon': lon[i][0],
-                'alt': alt[i][0],
-                'vn': vn[i][0],
-                've': ve[i][0],
-                'vd': vd[i][0],
-                'phi': roll[i][0]*d2r,
-                'the': pitch[i][0]*d2r,
+                'time': timestamp[i],
+                'lat': lat[i],
+                'lon': lon[i],
+                'alt': alt[i],
+                'vn': vn[i],
+                've': ve[i],
+                'vd': vd[i],
+                'phi': roll[i]*d2r,
+                'the': pitch[i]*d2r,
                 'psi': psi,
                 'psix': psix,
                 'psiy': psiy,
-                'p_bias': gbx[i][0],
-                'q_bias': gby[i][0],
-                'r_bias': gbz[i][0],
-                'ax_bias': abx[i][0],
-                'ay_bias': aby[i][0],
-                'az_bias': abz[i][0]
+                'p_bias': gbx[i],
+                'q_bias': gby[i],
+                'r_bias': gbz[i],
+                'ax_bias': abx[i],
+                'ay_bias': aby[i],
+                'az_bias': abz[i]
             }
             result['filter'].append(filter)
 
@@ -203,117 +203,117 @@ def load(h5_filename, recalibrate=None):
     #                 result['filter_post'].append(nav)
 
     print('Pilot input mapping:', pilot_mapping)
-    timestamp = data['/sensors/pilot/timestamp'][()].astype(float)
-    ch0 = data['/sensors/pilot/channel[0]'][()].astype(float)
-    ch1 = data['/sensors/pilot/channel[1]'][()].astype(float)
-    ch2 = data['/sensors/pilot/channel[2]'][()].astype(float)
-    ch3 = data['/sensors/pilot/channel[3]'][()].astype(float)
-    ch4 = data['/sensors/pilot/channel[4]'][()].astype(float)
-    ch5 = data['/sensors/pilot/channel[5]'][()].astype(float)
-    ch6 = data['/sensors/pilot/channel[6]'][()].astype(float)
-    ch7 = data['/sensors/pilot/channel[7]'][()].astype(float)
+    timestamp = data['/sensors/pilot/timestamp'][()]
+    ch0 = data['/sensors/pilot/channel[0]'][()]
+    ch1 = data['/sensors/pilot/channel[1]'][()]
+    ch2 = data['/sensors/pilot/channel[2]'][()]
+    ch3 = data['/sensors/pilot/channel[3]'][()]
+    ch4 = data['/sensors/pilot/channel[4]'][()]
+    ch5 = data['/sensors/pilot/channel[5]'][()]
+    ch6 = data['/sensors/pilot/channel[6]'][()]
+    ch7 = data['/sensors/pilot/channel[7]'][()]
     result['pilot'] = []
     for i in range(len(timestamp)):
         if pilot_mapping == 'Aura3':
             pilot = {
-                'time': timestamp[i][0],
-                'auto_manual': ch0[i][0],
-                'throttle_safety': ch1[i][0],
-                'throttle': ch2[i][0],
-                'aileron': ch3[i][0],
-                'elevator': ch4[i][0],
-                'rudder': ch5[i][0],
-                'flaps': ch6[i][0],
-                'aux1': ch7[i][0],
+                'time': timestamp[i],
+                'auto_manual': ch0[i],
+                'throttle_safety': ch1[i],
+                'throttle': ch2[i],
+                'aileron': ch3[i],
+                'elevator': ch4[i],
+                'rudder': ch5[i],
+                'flaps': ch6[i],
+                'aux1': ch7[i],
                 'gear': 0
             }
         elif pilot_mapping == 'APM2':
             pilot = {
-                'time': timestamp[i][0],
-                'aileron': ch0[i][0],
-                'elevator': -ch1[i][0],
-                'throttle': ch2[i][0],
-                'rudder': ch3[i][0],
-                'gear': ch4[i][0],
-                'flaps': ch5[i][0],
-                'aux1': ch6[i][0],
-                'auto_manual': ch7[i][0],
+                'time': timestamp[i],
+                'aileron': ch0[i],
+                'elevator': -ch1[i],
+                'throttle': ch2[i],
+                'rudder': ch3[i],
+                'gear': ch4[i],
+                'flaps': ch5[i],
+                'aux1': ch6[i],
+                'auto_manual': ch7[i],
                 'throttle_safety': 0.0
             }
         else:
             pilot = {}
         result['pilot'].append(pilot)
 
-    timestamp = data['/actuators/act/timestamp'][()].astype(float)
-    ail = data['/actuators/act/aileron_norm'][()].astype(float)
-    elev = data['/actuators/act/elevator_norm'][()].astype(float)
-    thr = data['/actuators/act/throttle_norm'][()].astype(float)
-    rud = data['/actuators/act/rudder_norm'][()].astype(float)
-    gear = data['/actuators/act/channel5_norm'][()].astype(float)
-    flaps = data['/actuators/act/flaps_norm'][()].astype(float)
-    aux1 = data['/actuators/act/channel7_norm'][()].astype(float)
-    auto_manual = data['/actuators/act/channel8_norm'][()].astype(float)
+    timestamp = data['/actuators/act/timestamp'][()]
+    ail = data['/actuators/act/aileron_norm'][()]
+    elev = data['/actuators/act/elevator_norm'][()]
+    thr = data['/actuators/act/throttle_norm'][()]
+    rud = data['/actuators/act/rudder_norm'][()]
+    gear = data['/actuators/act/channel5_norm'][()]
+    flaps = data['/actuators/act/flaps_norm'][()]
+    aux1 = data['/actuators/act/channel7_norm'][()]
+    auto_manual = data['/actuators/act/channel8_norm'][()]
     result['act'] = []
     for i in range(len(timestamp)):
         act = {
-            'time': timestamp[i][0],
-            'aileron': ail[i][0],
-            'elevator': elev[i][0],
-            'throttle': thr[i][0],
-            'rudder': rud[i][0],
-            'gear': gear[i][0],
-            'flaps': flaps[i][0],
-            'aux1': aux1[i][0],
-            'auto_manual': auto_manual[i][0]
+            'time': timestamp[i],
+            'aileron': ail[i],
+            'elevator': elev[i],
+            'throttle': thr[i],
+            'rudder': rud[i],
+            'gear': gear[i],
+            'flaps': flaps[i],
+            'aux1': aux1[i],
+            'auto_manual': auto_manual[i]
         }
         result['act'].append(act)
 
 
-    timestamp = data['/autopilot/timestamp'][()].astype(float)
+    timestamp = data['/autopilot/timestamp'][()]
     master = data['/autopilot/master_switch'][()]
     pass_through = data['/autopilot/pilot_pass_through'][()]
-    hdg = data['/autopilot/groundtrack_deg'][()].astype(float)
-    roll = data['/autopilot/roll_deg'][()].astype(float)
-    alt = data['/autopilot/altitude_msl_ft'][()].astype(float)
-    pitch = data['/autopilot/pitch_deg'][()].astype(float)
-    speed = data['/autopilot/airspeed_kt'][()].astype(float)
-    ground = data['/autopilot/altitude_ground_m'][()].astype(float)
+    hdg = data['/autopilot/groundtrack_deg'][()]
+    roll = data['/autopilot/roll_deg'][()]
+    alt = data['/autopilot/altitude_msl_ft'][()]
+    pitch = data['/autopilot/pitch_deg'][()]
+    speed = data['/autopilot/airspeed_kt'][()]
+    ground = data['/autopilot/altitude_ground_m'][()]
     result['ap'] = []
     for i in range(len(timestamp)):
-        hdgx = math.cos(hdg[i][0]*d2r)
-        hdgy = math.sin(hdg[i][0]*d2r)
+        hdgx = math.cos(hdg[i]*d2r)
+        hdgy = math.sin(hdg[i]*d2r)
         ap = {
-            'time': timestamp[i][0],
-            'master_switch': master[i][0],
-            'pilot_pass_through': pass_through[i][0],
-            'hdg': hdg[i][0],
+            'time': timestamp[i],
+            'master_switch': master[i],
+            'pilot_pass_through': pass_through[i],
+            'hdg': hdg[i],
             'hdgx': hdgx,
             'hdgy': hdgy,
-            'roll': roll[i][0],
-            'alt': alt[i][0],
-            'pitch': pitch[i][0],
-            'speed': speed[i][0],
-            'ground': ground[i][0]
+            'roll': roll[i],
+            'alt': alt[i],
+            'pitch': pitch[i],
+            'speed': speed[i],
+            'ground': ground[i]
         }
         result['ap'].append(ap)
 
-    timestamp = data['/sensors/health/timestamp'][()].astype(float)
-    load_avg = data['/sensors/health/system_load_avg'][()].astype(float)
-    avionics_vcc = data['/sensors/health/avionics_vcc'][()].astype(float)
-    main_vcc = data['/sensors/health/main_vcc'][()].astype(float)
-    cell_vcc = data['/sensors/health/cell_vcc'][()].astype(float)
-    main_amps = data['/sensors/health/main_amps'][()].astype(float)
-    total_mah = data['/sensors/health/total_mah'][()].astype(float)
+    timestamp = data['/sensors/health/timestamp'][()]
+    load_avg = data['/sensors/health/system_load_avg'][()]
+    avionics_vcc = data['/sensors/health/avionics_vcc'][()]
+    main_vcc = data['/sensors/health/main_vcc'][()]
+    cell_vcc = data['/sensors/health/cell_vcc'][()]
+    main_amps = data['/sensors/health/main_amps'][()]
+    total_mah = data['/sensors/health/total_mah'][()]
     result['health'] = []
     for i in range(len(timestamp)):
         health = {
-            'time': timestamp[i][0],
-            'load_avg': load_avg[i][0],
-            'avionics_vcc': avionics_vcc[i][0],
-            'main_vcc': main_vcc[i][0],
-            'cell_vcc': cell_vcc[i][0],
-            'main_amps': main_amps[i][0],
-            'total_mah': total_mah[i][0]
+            'time': timestamp[i],
+            'load_avg': load_avg[i],
+            'avionics_vcc': avionics_vcc[i],
+            'main_vcc': main_vcc[i],
+            'cell_vcc': cell_vcc[i],
+            'main_amps': main_amps[i],
+            'total_mah': total_mah[i]
         }
         result['health'].append(health)
 
