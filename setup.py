bed36e57543e762640a9b5bc4dfb920d0cb85ff3
Curtis L. Olson <curtolson@flightgear.org>
2021-04-14 11:14:13 -0500

    Small bit more rebranding, reorg.  My high hopes at using shared namespace packages in python didn't work out due to my own lack of understanding why they weren't working for me and running out of time.

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='rcUAS_flightdata',
-      version='1.3',
-      description='Flight data management module',
-      author='Curtis L. Olson',
-      author_email='curtolson@flightgear.org',
-      url='https://github.com/RiceCreekUAS',
-      #package_dir = {'': 'aurauas'},
-      #packages = ['aurauas_flightdata']
-      packages = find_packages()
-     )
+setup(
+    name='rcUAS_flightdata',
+    version='1.3',
+    description='Flight data management module',
+    author='Curtis L. Olson',
+    author_email='curtolson@flightgear.org',
+    url='https://github.com/RiceCreekUAS',
+    packages = ['rcUAS_flightdata']
+)
